6da32b2b496d4eaadb1e22c48006f81235d72c0d
AkashAich1999 <108985323+AkashAich1999@users.noreply.github.com>
2024-05-25 17:00:51 +0530

    Using app.use(express.json());

--- backend/server.js
+++ backend/server.js
@@ -7,12 +7,13 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req,res) => {
-    // root route http://localhost:5000/
-    res.send("Hello World !!");
-});
+// app.get("/", (req,res) => {
+//     // root route http://localhost:5000/
+//     res.send("Hello World !!");
+// });
 
 // middlewares
+app.use(express.json());  // to parse the incoming requests with JSON payloads (from req.body)
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
