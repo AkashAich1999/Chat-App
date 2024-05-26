f360b983d103df22b3cc115b627deb50a73f264b
AkashAich1999 <108985323+AkashAich1999@users.noreply.github.com>
2024-05-26 19:07:49 +0530

    Logout Endpoint

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -77,5 +77,11 @@ export const login = async (req,res) => {
 };
 
 export const logout = (req,res) => {
-    res.send("Logout User");
+    try {
+        res.cookie("jwt","",{maxAge:0});
+        res.status(200).json({message:"Logged Out Successfully"});
+    } catch (error) {
+        console.log("Error in Logout Controller", error.message);
+        res.status(500).json({error:"Internal Server Error"});        
+    }
 };
