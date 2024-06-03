ff5fffce7f26b941ca35f6c76d7754b20bc2d1d8
AkashAich1999 <108985323+AkashAich1999@users.noreply.github.com>
2024-06-03 08:24:07 +0530

    Automatic Scroll-Up Functionality

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,22 +1,36 @@
+import { useEffect, useRef } from "react";
 import useGetMessages from "../../hooks/useGetMessages";
 import MessageSkeleton from "../skeletons/MessageSkeleton";
 import Message from "./Message";
 
 const Messages = () => {
-	const {messages,loading} = useGetMessages();
-	console.log("messages", messages);
-	return (
-		<div className='px-4 flex-1 overflow-auto'>
-			{!loading && messages.length > 0 && messages.map((message) => (
-				<Message key={message._id} message={message} />
-			))}
+  const { messages, loading } = useGetMessages();
+  const lastMessageRef = useRef();
+  useEffect(() => {
+    setTimeout(() => {
+      lastMessageRef.current?.scrollIntoView({ behavior: "smooth" });
+    }, 100);
+  }, [messages]);
 
-			{loading && [...Array(3)].map((_,idx) => <MessageSkeleton key={idx} />) }
+  // console.log("messages", messages);
+  return (
+    <div className="px-4 flex-1 overflow-auto">
+      {!loading &&
+        messages.length > 0 &&
+        messages.map((message) => (
+          <div key={message._id} ref={lastMessageRef}>
+            <Message message={message} />
+          </div>
+        ))}
 
-			{!loading && messages.length === 0 && (
-				<p className="text-center text-white">Send a message to start the conversation</p>
-			)}
-		</div>
-	);
+      {loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
+
+      {!loading && messages.length === 0 && (
+        <p className="text-center text-white">
+          Send a message to start the conversation
+        </p>
+      )}
+    </div>
+  );
 };
 export default Messages;
